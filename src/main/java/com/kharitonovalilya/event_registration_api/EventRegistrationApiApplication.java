9777832583e776b5e4e2d3b0c67db85dab0e46d8
Lilya <kharitonovalilya1@gmail.com>
2026-04-30 17:54:36 +0300

    inital commit

---
+++ src/main/java/com/kharitonovalilya/event_registration_api/EventRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.kharitonovalilya.event_registration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kharitonovalilya/event_registration_api/EventRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kharitonovalilya.event_registration_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
